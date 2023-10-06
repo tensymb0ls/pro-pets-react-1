362abd4f74df3341e12aa9bf782c9cbfe6956c89
tensymb0ls <tensym8ols@gmail.com>
2023-10-06 17:43:34 +0300

    дл

--- src/Pages/Dashboard/Dashboard.js
+++ src/Pages/Dashboard/Dashboard.js
@@ -1,3 +1,26 @@
+import { useState } from "react"
+import { useEffect } from "react"
+
 export const Dashboard = () => {
-    return <div>Dashboard</div>
+
+    //  state hook
+    const [count, setCount] = useState(0);
+    const [products, setProducts] = useState('Default');
+    const [toDo, setToDo] = useState([{ text: 'Buy beer' }]);
+    const HandleClickElement = () => {
+        setCount(count+1);
+    }
+    //  effect hook
+    useEffect(() => {
+        alert('Component mount');
+    }, [count]);
+    return <div>
+        <p>counter {count}</p>
+        <p>{products}</p>
+        <ul>
+            {toDo.map(item => <li key={item.text}>{item.text}</li>)}
+        </ul>
+        <button onClick={HandleClickElement}>Click me</button>
+
+    </div>
 } 
--- src/Pages/PrivateRoute/PrivateRoute.page.js
+++ src/Pages/PrivateRoute/PrivateRoute.page.js
@@ -2,7 +2,7 @@ import { useContext } from "react";
 import { Navigate, Outlet } from "react-router-dom";
 import { UserContext } from "../../contexts/user.context";
 
-const PrivateRoute = () => {
+const PrivateRoute = ({children}) => {
 
     // Fetching the user from the user context.
     const { user } = useContext(UserContext);
@@ -11,7 +11,7 @@ const PrivateRoute = () => {
     // If the user is not logged in we are redirecting them
     // to the login page. Otherwise we are letting them to
     // continue to the page as per the URL using <Outlet />.
-    return !user ? <Navigate to={redirectLoginUrl} /> : <Outlet />;
+    return !user ? <Navigate to={redirectLoginUrl} /> : children;
 }
 
 export default PrivateRoute;
