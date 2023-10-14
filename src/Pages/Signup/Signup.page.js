f762ab7f2800e33b0e08d535b7b289754fdfc524
tensymb0ls <tensym8ols@gmail.com>
2023-10-14 14:43:13 +0300

    asd

--- src/Pages/Signup/Signup.page.js
+++ src/Pages/Signup/Signup.page.js
@@ -3,11 +3,19 @@ import { Footer } from "../../Components/Footer/Footer";
 import { SignUpForm } from "../../Components/SignUpForm/SignUpForm";
 
 const Signup = () => {
+<<<<<<< Updated upstream
     
 
     return <div className="bg-cats h-screen flex flex-col justify-between">
         <Header />
         <SignUpForm/>
+=======
+
+
+    return <div className="bg-cats h-screen flex flex-col justify-between">
+        <Header />
+        <SignUpForm />
+>>>>>>> Stashed changes
         <Footer />
     </div>
 
